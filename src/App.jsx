533684af90384d2589b09e657255ba2a077e0499
box-7 <hayainada1981@yahoo.co.jp>
2024-10-10 07:10:03 +0900

    MVP1 完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import { useState } from 'react'
 import './App.css'
 import supabase from './utils/supabase'
-
+import { useEffect } from 'react'
 
 function App() {
-        console.log("supabase 222", supabase)
+        const [data, setData] = useState(null);
+        const [totalTime, setTotalTime] = useState(0);
+        const [isLoading, setIsLoading] = useState(true);
+        useEffect(() => {
+                const fetchData = async () => {
+                        try {
+                                const { data } = await supabase.from('study-record').select('*');
+                                setData(data);
+                                setIsLoading(false);
+                        } catch (error) {
+                                console.error('Error fetching data:', error);
+                        }
+                };
+                // fetchData();
+                fetchData().then(data => {
+                        const calculatedTotalTime = data.reduce((acc, record) => acc + parseInt(record.time), 0);
+                        setTotalTime(calculatedTotalTime);
+                });
+        }, []);
+
+        // useEffect(() => {
+        //         if (data) {
+        //                 const calculatedTotalTime = data.reduce((acc, record) => acc + parseInt(record.time), 0);
+        //                 setTotalTime(calculatedTotalTime);
+        //         }
+        // }, [data]);
 
         const [studyContent, setStudyContent] = useState('');
         const [studyHour, setStudyHour] = useState(0);
@@ -29,8 +54,6 @@ function App() {
                 setRecords(updateRecords)
         };
 
-        const totalTime = records.reduce((acc, record) => acc + parseInt(record.time), 0);
-
         return (
                 <>
                         <h1>学習記録一覧</h1>
@@ -40,10 +63,28 @@ function App() {
                         <p>入力されている学習時間: {studyHour}時間</p>
                         <button onClick={onClickSetRecord}>登録</button>
 
-                        {records.map((record, index) => (
+                        {/* {records.map((record, index) => ( */}
+                        {/* {data?.map((record, index) => (
                                 <p  key={index}>{record.title} {record.time}時間</p>
-                        ))}
+                        ))} */}
 
+                        <div>
+                                {isLoading ? (
+                                        <p>Loading...</p>
+                                ) : (
+                                        data && data.length > 0 ? (
+                                        <div>
+                                                {/* データを表示する部分 */}
+                                                {data.map((record, index) => (
+                                                // <div key={item.id}>{item.name}</div>
+                                                        <p  key={index}>{record.title} {record.time}時間</p>
+                                                ))}
+                                        </div>
+                                        ) : (
+                                        <p>データがありません</p>
+                                        )
+                                )}
+                        </div>
                         {error && <p>{error}</p>}
 
                         <h2>合計時間: {totalTime} / 1000(h)</h2>
@@ -53,3 +94,9 @@ function App() {
 
 export default App
 
+        // const totalTime = data.reduce((acc, record) => acc + parseInt(record.time), 0);
+
+        // console.log("data",data);
+        // console.log("supabase.fromの確認");
+        // const test = async supabase.from("study-record").select("*");
+        // console.log(test.data);
--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,35 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
 
-console.log("import.meta.env", import.meta.env);
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =  import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// supabaseのHPのAPISettingsから、ProjectURLとAPI Keysを取得
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log("supabaseUrl 000", supabaseUrl);
-
-const supabase = createClient(supabaseUrl, supabaseKey)
-console.log("supabase 111", supabase);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
 
 
-
-
-
-// require('dotenv').config();
-
-
-// const supabase = createClient(
-//         import.meta.env.VITE_SUPABASE_URL,
-//         import.meta.env.VITE_SUPABASE_ANON_KEY
-// );
-// console.log("import.meta.env.VITE_SUPABASE_URL");
-// console.log(import.meta.env.VITE_SUPABASE_URL);
-
-// export const supabase = createClient(
-//         import.meta.env.VITE_SUPABASE_URL,
-//         process.env.SUPABASE_ANON_KEY
-// );
-// Create a single supabase client for interacting with your database
-        // export const supabase = createClient(
-        //         'https://xyzcompany.supabase.co', 'public-anon-key')
+// .envのディレクトリの階層が誤っていたためエラーになった
