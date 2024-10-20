b391739c48054786a05fe718deca92403a32ed89
box-7 <hayainada1981@yahoo.co.jp>
2024-10-20 21:25:13 +0900

    vite.config.jsを修正

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  define: {
-        'process.env': import.meta.env
-  }
+  // defineオプションは不要
 })
+
+
+
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+//   // 環境変数をロード
+//   const env = loadEnv(mode, process.cwd(), '')
+
+//   return {
+//     plugins: [react()],
+//     define: {
+//       'process.env': env
+//     }
+//   }
+// })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+
+//   define: {
+//         'process.env': import.meta.env
+//   }
+// })
