35f8cec6cc1b9c3833bdb34e19b059345680b8df
box-7 <hayainada1981@yahoo.co.jp>
2024-10-20 20:44:09 +0900

    supabase.jsで環境変数が来ているか確認

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -7,6 +7,19 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+// 環境変数が設定されているかどうかを確認
+if (supabaseUrl) {
+        console.log('Supabase URL is set');
+} else {
+        console.log('Supabase URL is not set');
+}
+
+if (supabaseKey) {
+        console.log('supabaseKey is set');
+} else {
+        console.log('supabaseKey is not set');
+}
+
 export default supabase;
 
 
